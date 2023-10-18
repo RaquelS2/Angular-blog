e14f958394fbd16d154327eed8c6d8a2d64dced5
RaquelS2 <raquelpereirasoaress2@gmail.com>
2023-10-18 09:59:14 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"NOVO HOMEM DE FERRO EM 3D",
+        "description": "Marvel anuncia novo homem de ferro",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=506&aspectRatio=2.00&format=jpeg"
+    },
+    {
+        "id":"2",
+        "title":"Nova série anunciada no Disney +",
+        "description": "ola ola ola",
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+    {
+        "id":"3",
+        "title":"Nova série anunciada no Disney +",
+        "description": "ola ola ola",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRXWGyKOjzA0Mu_tmlXvX2uuHzIp7d3A08qQ&usqp=CAU"
+
+    },
+    
+    {
+        "id":"4",
+        "title":"Nova série anunciada no Disney +",
+        "description": "ola ola ola",
+        "photoCover":"https://media.vandalsports.com/i/640x360/3-2021/20213593638_1.jpg"
+    },
+    
+]
