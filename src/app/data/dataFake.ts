a5b759b67791ac3f7a219af5ea447e1df1fa5602
RaquelS2 <raquelpereirasoaress2@gmail.com>
2023-10-18 14:22:22 -0300

    alterações simples

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -7,22 +7,21 @@ export const dataFake = [
     },
     {
         "id":"2",
-        "title":"Nova série anunciada no Disney +",
-        "description": "ola ola ola",
+        "title":"CAPITÃO AMÉRICA",
+        "description": "Filme do Capitão America é um dos mais aguardados da Netiflix.",
         "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
     },
     {
         "id":"3",
-        "title":"Nova série anunciada no Disney +",
-        "description": "ola ola ola",
+        "title":"PANTERA NEGRA ",
+        "description": "Pantera negra estreia nos cinemas de todo o Brasil",
         "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRXWGyKOjzA0Mu_tmlXvX2uuHzIp7d3A08qQ&usqp=CAU"
-
     },
-    
+
     {
         "id":"4",
-        "title":"Nova série anunciada no Disney +",
-        "description": "ola ola ola",
+        "title":"X-Men",
+        "description": "The Mutants”: foi revelado o título do proximo filme dos X-Men no MCU",
         "photoCover":"https://media.vandalsports.com/i/640x360/3-2021/20213593638_1.jpg"
     },
     
